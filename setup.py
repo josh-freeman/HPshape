ba23e50b0be59103dfd176f9d1d63431d1714c84
jfreeman <joshua.freeman@epfl.ch>
2022-04-07 12:09:44 +0200

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Named Entities Extraction',
+    version='0.0',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://github.com/josh-freeman/NLPProject',
+    license='MIT?',
+    author='jfreeman',
+    author_email='joshua.freeman@epfl.ch',
+    description='Given a text, this program will be able to extract named entities',
+)
