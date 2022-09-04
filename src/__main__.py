9d4f19427e3f7865e5faa03c7575c5f36742f722
jfreeman <joshua.freeman@epfl.ch>
2022-09-04 16:10:27 +0200

    TODOs

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,11 @@
 import torch
-from util.constants import BOOK_NAMES, RESOURCES_DIRNAME, CURR_BOOK_NR, BATCH_SIZE, D, MODEL_NAME, CHECKPOINT_DIRNAME, \
+from torch import optim
+from torch.utils.data import DataLoader
+
+from util.constants import BOOK_NAMES, RESOURCES_DIRNAME, BATCH_SIZE, D, MODEL_NAME, CHECKPOINT_DIRNAME, \
     LEARNING_RATE, EPOCHS, CRITERION, C
 from util.model import NN, train_model
-from util.pre_proc import pre_proc, remove_page_lines_hp
-from torch.utils.data import DataLoader
-from torch import nn, optim
+from util.pre_proc import pre_proc
 from util.util import absolute_path, build_data_set
 
 device = torch.device(('cpu', 'cuda')[torch.cuda.is_available()])
@@ -25,18 +26,13 @@ def main():
         train_model(model, CRITERION, opt, data_loader, EPOCHS)
         (_, list_of_samples) = pre_proc(path, C, vocab)
 
-    # TODO: do this with the Vocab of the first book for all harry potter books
-
     torch.save(model, absolute_path(
         f"/{CHECKPOINT_DIRNAME}/{MODEL_NAME}"))
     # retrieve representation via "encode" function
-    # TODO : separate test file in which we load the ckpt to do the tests.
     # TODO : use validation
-    # TODO :  after each epoch (or in case of KeyboardInterrupt), save.
+    # TODO : after each epoch (or in case of KeyboardInterrupt), save.
 
 
 if __name__ == '__main__':
     # python -m spacy init vectors en ./examples/gensim-model.txt ./examples/spacy
-    # TODO : use remove_page_ligns_hp, and remove_consecutive_blank_lines
-    # TODO opt: use tqdm for progress bar at each iteration of eopchs during training.
     main()
