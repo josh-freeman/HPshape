c649e5475c12a454e1644929d3aa4f584cf789a1
Joshua Freeman <47647715+josh-freeman@users.noreply.github.com>
2022-10-02 15:54:32 +0100

    Add files via upload

---
+++ deploy_and_train.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt get update
+git clone https://github.com/josh-freeman/HPshape.git
+
+cd HPshape
+mkdir examples
+
+declare -a list=("Book%201%20-%20The%20Philosopher's%20Stone.txt" "Book%202%20-%20The%20Chamber%20of%20Secrets.txt" "Book%203%20-%20The%20Prisoner%20of%20Azkaban.txt" "Book%204%20-%20The%20Goblet%20of%20Fire.txt")
+
+# About to iterate through list of titles
+for title in ${list[@]}; do
+   modified="${title//%20/ }"
+
+   # unescaping spaces from url format in the title
+   echo Downloading : examples/$modified
+
+   curl --output "examples/$modified" --url https://raw.githubusercontent.com/formcept/whiteboard/master/nbviewer/notebooks/data/harrypotter/$title
+
+done
+
+mkdir ckpt
+
+# at this point, everything is ready to launch training.
+
+
+# run the model from HPshape/main/__main__.py. From here, each command is vital. Hence we activate -e
+
+
+set -Eeuo pipefail
+
+sudo apt install python3-pip
+pip3 install requirements.txt
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download en_core_web_trf
+
+python3 -m src.__main__
