1a220550192d2bade545033b61099b15a0f2bf60
jfreeman <joshua.freeman@epfl.ch>
2022-10-03 08:31:47 +0100

    complete titles

--- deploy_and_train.sh
+++ deploy_and_train.sh
@@ -6,11 +6,12 @@ git clone https://github.com/josh-freeman/HPshape.git
 cd HPshape
 mkdir examples
 
-declare -a list=("Book%201%20-%20The%20Philosopher's%20Stone.txt" "Book%202%20-%20The%20Chamber%20of%20Secrets.txt" "Book%203%20-%20The%20Prisoner%20of%20Azkaban.txt" "Book%204%20-%20The%20Goblet%20of%20Fire.txt")
+declare -a list=("Book%201%20-%20The%20Philosopher's%20Stone.txt" "Book%202%20-%20The%20Chamber%20of%20Secrets.txt" "Book%203%20-%20The%20Prisoner%20of%20Azkaban.txt" "Book%204%20-%20The%20Goblet%20of%20Fire.txt" "Book%205%20-%20The%20Order%20of%20the%20Phoenix.txt" "Book%206%20-%20The%20Half%20Blood%20Prince.txt" "Book%207%20-%20The%20Deathly%20Hallows.txt")
 
 # About to iterate through list of titles
 for title in ${list[@]}; do
    modified="${title//%20/ }"
+   printf
 
    # unescaping spaces from url format in the title
    echo Downloading : examples/$modified
@@ -26,7 +27,7 @@ mkdir ckpt
 
 # run the model from HPshape/main/__main__.py. From here, each command is vital. Hence we activate -e
 
-
+set -e
 set -Eeuo pipefail
 
 sudo apt install python3-pip
